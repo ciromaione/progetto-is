6b7b396ed0cb4eaee9aef45bec89c7de205d6f31
ciro <ciro@manjaro>
2019-12-23 11:43:54 +0100

    Inizializzazione progetto server

---
+++ src/java/ejb/DataManagerEJB.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author ciro
+ */
+@Stateless
+public class DataManagerEJB {
+
+//    public void addDish(Dish dish);
+//    public void removeDish(Dish dish);
+//    
+    //WebServices
+//    public Collection<Dish> getMenu();
+//    public void addOrder(Order order);
+    
+}
