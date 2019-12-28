cadd5a8edce05ebe2e0a8b89645e2f46d07bda17
ciro <ciro@manjaro>
2019-12-28 17:47:00 +0100

    aggiunta singleton per conservare gli ordini attivi

---
+++ src/java/managers/OrdineManager.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managers;
+
+import entities.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import javax.ejb.Singleton;
+
+/**
+ *
+ * @author ciro
+ */
+@Singleton
+public class OrdineManager {
+
+    private HashMap<Integer, Ordine> ordiniAttivi;
+
+    public OrdineManager() {
+        this.ordiniAttivi = new HashMap<Integer, Ordine>();
+    }
+    
+    public void addToOrdiniAttivi(Ordine ordine) {
+        if(ordine == null)
+            throw new RuntimeException("Ordine Null!");
+        if(ordine.getPiatti() == null)
+                throw new RuntimeException("Ordine vuoto!");
+        
+        if(ordiniAttivi.containsKey(ordine.getId())) {
+            Collection<Piatto> piatti = ordiniAttivi
+                    .get(ordine.getId())
+                    .getPiatti();
+            Integer totale = ordiniAttivi
+                    .get(ordine.getId())
+                    .getTotaleCent();
+            piatti.addAll(ordine.getPiatti());
+            totale += ordine.getTotaleCent();
+            ordiniAttivi.get(ordine.getId())
+                    .setPiatti(piatti);
+            ordiniAttivi.get(ordine.getId())
+                    .setTotaleCent(totale);
+        }
+        else
+            ordiniAttivi.put(ordine.getId(), ordine);
+    }
+    
+    public void removeFromOrdiniAttivi(Integer id) {
+        ordiniAttivi.remove(id);
+    }
+    
+}
