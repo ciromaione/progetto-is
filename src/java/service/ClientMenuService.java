e42f3e60d265de5d2a918f73c3849494fba9a96d
ciromaione <ciroma809@gmail.com>
2020-01-02 17:41:09 +0100

    modifiche varie

--- src/java/service/ClientMenuService.java
+++ src/java/service/ClientMenuService.java
@@ -12,7 +12,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import model.dao.PiattoDAO;
 import model.entities.Piatto;
 import model.managers.ClienteManager;
 
