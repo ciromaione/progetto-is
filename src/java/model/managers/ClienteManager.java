579eeedcd8cbdc2293fee7a275defbd7e545390e
ciromaione <ciroma809@gmail.com>
2020-01-02 14:42:18 +0100

    aggiunta client manager

---
+++ src/java/model/managers/ClienteManager.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.managers;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import model.entities.Ingrediente;
+import model.entities.Piatto;
+
+/**
+ *
+ * @author ciro
+ */
+@Stateless
+public class ClienteManager {
+
+    @Inject
+    Connection conn;
+    
+    public List<Piatto> findAll() {
+        try {
+            PreparedStatement ps = conn.prepareStatement(""
+                    + "SELECT id, nome, categoria, prezzo_cent, foto "
+                    + "FROM piatto");
+            
+            ResultSet rs = ps.executeQuery();
+            
+            ArrayList<Piatto> all = new ArrayList<>();
+            while(rs.next()) {
+                Piatto piatto = new Piatto();
+                piatto.setId(rs.getInt(1));
+                piatto.setNome(rs.getString(2));
+                piatto.setCategoria(rs.getString(3));
+                piatto.setPrezzoCent(rs.getInt(4));
+                piatto.setFoto(rs.getString(5));
+                piatto.setIngredienti(getIngredientiList(piatto.getId(), "piattoXing"));
+                piatto.setIngredientiAggiungibili(getIngredientiList(piatto.getId(), "piattoXing_ins"));
+                piatto.setIngredientiRimovibili(getIngredientiList(piatto.getId(), "piattoXing_rem"));
+                all.add(piatto);
+            }
+            return all;
+            
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    public List<String> findCategorie() {
+        try {
+            PreparedStatement ps = conn.prepareStatement(""
+                    + "SELECT DISTINCT categoria "
+                    + "FROM piatto");
+            
+            ResultSet rs = ps.executeQuery();
+            
+            ArrayList<String> all = new ArrayList<>();
+            while(rs.next()) 
+                all.add(rs.getString(1));
+            
+            return all;
+            
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    private Collection<Ingrediente> getIngredientiList(int id, String table) throws SQLException {
+        PreparedStatement ps = conn.prepareStatement(""
+                + "SELECT i.id, i.nome, i.categoria, i.sovrapprezzo_cent "
+                    + "FROM ingrediente i, "+table+" pi "
+                    + "WHERE pi.id_piatto = ? AND i.id = pi.id_ingrediente");
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        ArrayList<Ingrediente> all = new ArrayList<>();
+        while (rs.next()) {
+            Ingrediente ing = new Ingrediente();
+            ing.setId(rs.getInt(1));
+            ing.setNome(rs.getString(2));
+            ing.setCategoria(rs.getString(3));
+            ing.setSovrapprezzoCent(rs.getInt(4));
+            all.add(ing);
+        }
+        return all;
+    }
+}
--- src/java/model/managers/TitolareManager.java
+++ src/java/model/managers/TitolareManager.java
@@ -7,6 +7,10 @@ package model.managers;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -28,10 +32,39 @@ public class TitolareManager {
     }
     
     public void aggiungiPiatto(Piatto piatto) {
-        
+        try {
+            PreparedStatement ps = conn.prepareStatement(""
+                    + "INSERT INTO piatto (nome, categoria, prezzo_cent, foto) "
+                    + "VALUES (?, ?, ?, ?)", PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, piatto.getNome());
+            ps.setString(2, piatto.getCategoria());
+            ps.setInt(3, piatto.getPrezzoCent());
+            ps.setString(4, piatto.getFoto());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()) piatto.setId(rs.getInt(1));
+            else throw new RuntimeException();
+            
+            saveRelationship(piatto.getId(), piatto.getIngredienti(), "piattoXing");
+            saveRelationship(piatto.getId(), piatto.getIngredientiAggiungibili(), "piattoXing_ins");
+            saveRelationship(piatto.getId(), piatto.getIngredientiRimovibili(), "piattoXing_rem");
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
     }
     
     public void rimuoviPiatto(Integer id) {
+        try {
+            PreparedStatement ps = conn.prepareStatement(""
+                    + "DELETE FROM piatto "
+                    + "WHERE id = ?");
+            ps.setInt(1, id);
+            
+            ps.executeUpdate();
+            
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
         
     }
     
@@ -46,5 +79,19 @@ public class TitolareManager {
     
     public List<Piatto> guadagnoGiornaliero(Date data) {
         return null;
+    }
+    
+    
+    
+    private void saveRelationship(int id, Collection<Ingrediente> ing, String table) throws SQLException {
+        PreparedStatement ps;
+        for (Ingrediente i : ing) {
+            ps = conn.prepareStatement(""
+                    + "INSERT INTO "+table+" (id_ingrediente, id_piatto) "
+                    + "VALUES (?, ?)");
+            ps.setInt(1, i.getId());
+            ps.setInt(2, id);
+            ps.executeUpdate();
+        }
     }
 }
