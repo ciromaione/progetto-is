624fa1eb97f42f461be5b82171ff014a8884a443
ciromaione <ciroma809@gmail.com>
2020-01-03 15:04:03 +0100

    aggiunta servlet per visualizza ordini

---
+++ src/java/servlet/VisualizzaOrdiniServlet.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.managers.AuthenticationManager;
+import model.managers.OrdiniSingleton;
+
+/**
+ *
+ * @author ciro
+ */
+@WebServlet(name = "VisualizzaOrdiniServlet", urlPatterns = {"/ordini"})
+public class VisualizzaOrdiniServlet extends HttpServlet {
+    
+    @Inject
+    private OrdiniSingleton os;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        Integer authType = (Integer) request.getSession()
+                .getAttribute("authType");
+        if(authType == null) {
+            request.setAttribute("authType", AuthenticationManager.STAFF);
+            request.setAttribute("target", "ordini");
+            request.getRequestDispatcher("login.jsp")
+                    .forward(request, response);
+        }
+        else {
+            request.setAttribute("ordini", os.getOrdiniDaCompletare());
+            request.getRequestDispatcher("visualizzaOrdini.jsp")
+                    .forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
