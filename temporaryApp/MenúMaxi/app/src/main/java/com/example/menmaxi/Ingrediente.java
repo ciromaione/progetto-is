35e0c380642d4adbcf41052e2026ee45f8704b94
cristian1738 <gunterone00@gmail.com>
2020-01-17 10:56:49 +0100

    revert

---
+++ temporaryApp/MenúMaxi/app/src/main/java/com/example/menmaxi/Ingrediente.java
@@ -0,0 +1,12 @@
+package com.example.menmaxi;
+
+public class Ingrediente {
+
+    private String name;
+    private String category;
+
+    public Ingrediente(String name, String category){
+        this.name = name;
+        this.category = category;
+    }
+}
