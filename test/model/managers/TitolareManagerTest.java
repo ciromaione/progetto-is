64bc3b1cadebdca114a9c554764b054037d2fa44
ciromaione <ciroma809@gmail.com>
2020-01-19 12:47:00 +0100

    testing titolare manager: addPiatto

--- test/model/managers/TitolareManagerTest.java
+++ test/model/managers/TitolareManagerTest.java
@@ -6,6 +6,8 @@
 package model.managers;
 
 import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.HashMap;
 import java.util.Map;
 import javax.ejb.EJBException;
@@ -24,26 +26,87 @@ public class TitolareManagerTest {
     private final int[] ingAgg = null;
     private final int[] ingRim = null;
     
+    private Map<String, Object> properties;
+    
     public TitolareManagerTest() {
+        properties = new HashMap<String, Object>();
+        properties.put(EJBContainer.MODULES, new File("build/jar"));
     }
     
     @Test
     public void TC_TM_1() throws Exception {
-        Map<String, Object> properties = new HashMap<String, Object>();
-        properties.put(EJBContainer.MODULES, new File("build/jar"));
         EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer(properties);
         TitolareManager instance = (TitolareManager) container.getContext().lookup("java:global/classes/TitolareManager");
         
+        //TC_TM_1.1
         Piatto piatto = null;
         try {
             instance.aggiungiPiatto(piatto, ing, ingAgg, ingRim);
             fail("Eccezione non lanciata");
         } catch (EJBException ex) {
-            assertTrue(true);
         }
+        
+        //TC_TM_1.2
+        piatto = new Piatto();
+        piatto.setNome("Panino con porchetta e fonduta di cheddar caramellato");
+        try {
+            instance.aggiungiPiatto(piatto, ing, ingAgg, ingRim);
+            fail("Eccezione non lanciata");
+        } catch (EJBException ex) {
+        }
+        
+        //TC_TM_1.3
+        piatto = new Piatto();
+        piatto.setNome("Panino con Porchetta");
+        piatto.setFoto("Qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm.jpg");
+        try {
+            instance.aggiungiPiatto(piatto, ing, ingAgg, ingRim);
+            fail("Eccezione non lanciata");
+        } catch (EJBException ex) {
+        }
+        
+        //TC_TM_1.4
+        piatto = new Piatto();
+        piatto.setNome("Panino con Porchetta");
+        piatto.setFoto("Panino_con_porchetta.jpg");
+        piatto.setCategoria("pppppppaaaaannnnniiiiinnnnnooooo");
+        try {
+            instance.aggiungiPiatto(piatto, ing, ingAgg, ingRim);
+            fail("Eccezione non lanciata");
+        } catch (EJBException ex) {
+        }
+        
+        //TC_TM_1.5
+        piatto = new Piatto();
+        piatto.setNome("Panino con Porchetta");
+        piatto.setFoto("Panino_con_porchetta.jpg");
+        piatto.setCategoria("panino");
+        piatto.setPrezzoCent(-3);
+        try {
+            instance.aggiungiPiatto(piatto, ing, ingAgg, ingRim);
+            fail("Eccezione non lanciata");
+        } catch (EJBException ex) {
+        }
+        
+        //TC_TM_1.6
+        piatto = new Piatto();
+        piatto.setNome("Panino con Porchetta");
+        piatto.setFoto("Panino_con_porchetta.jpg");
+        piatto.setCategoria("panino");
+        piatto.setPrezzoCent(800);
+        piatto = instance.aggiungiPiatto(piatto, ing, ingAgg, ingRim);
+        try (Connection conn = ConnectionProducer.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(
+                    "SELECT * FROM piatto WHERE id = ?");
+            ps.setInt(1, piatto.getId());
+            assertTrue(ps.executeQuery().next());
+        }
+        
         
         container.close();
     }
+    
+   
 
     
     
